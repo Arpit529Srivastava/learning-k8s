9ba7c9bd37feeac5d567e21a7d5b79e0354b19ee
arpit529srivastava <arpitsrivastava529@gmail.com>
2025-01-13 03:54:56 +0530

    checking

--- main.go
+++ main.go
@@ -11,6 +11,6 @@ func main() {
 	log.Println("Server is running on http://localhost:8000")
 	err := http.ListenAndServe(":8000", nil)
 	if err != nil {
-		log.Fatalf("Error hehe starting server: %v", err)
+		log.Fatalf("Error starting server: %v", err)
 	}
 }
